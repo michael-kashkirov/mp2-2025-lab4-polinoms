2ffb19d61ac42d912eb2807968827d0fe1354caa
Michael Kashkirov <michaelsmacbook@MacBook-Air-Michael.local>
2026-04-29 20:25:21 +0300

    lab4 without build

--- build/CMakeFiles/polinoms_app.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for polinoms_app.
-# This may be replaced when dependencies are built.
--- build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/michaelsmacbook/mp2-2025-lab4-polinoms/build/_deps/googletest-src/googletest
-# Build directory: /Users/michaelsmacbook/mp2-2025-lab4-polinoms/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
